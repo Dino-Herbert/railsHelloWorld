68d7cb0673d90861e99e254ddc05211536e0a6a9
Dino-Herbert <dherbert5764@gmail.com>
2017-07-14 23:40:19 -0400

    Modify route to run controller action

--- hello_world/app/controllers/application_controller.rb
+++ hello_world/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hola Mundo!"
+  end
+
+  def goodbye
+    render html: "See Ya Later, Goodbye, World!"
+  end
 end
